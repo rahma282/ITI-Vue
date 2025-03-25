3b5ae1e1a9969df7075f8fa64c9493c12fa78486
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-03-25 23:25:41 +0200

    d1

---
+++ Day1/task1/book.js
@@ -0,0 +1,133 @@
+books = [
+  { 
+    id: 1,
+    author: "Chinua Achebe",
+    imageLink: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/Things_Fall_Apart",
+    pages: 209,
+    title: "Things Fall Apart",
+    isbn: "978-0385474542",
+    category: "Fiction",
+    price: 15.99
+  },
+  {
+    id: 2,
+    author: "Hans Christian Andersen",
+    imageLink: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.",
+    pages: 784,
+    title: "Fairy Tales",
+    isbn: "978-0140448955",
+    category: "Children's Literature",
+    price: 12.99
+  },
+  {
+    id: 3,
+    author: "Dante Alighieri",
+    imageLink: "https://images.unsplash.com/photo-1509021436665-8f07dbf5bf1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/Divine_Comedy",
+    pages: 928,
+    title: "The Divine Comedy",
+    isbn: "978-0142437223",
+    category: "Classic",
+    price: 18.50
+  },
+  {
+    id: 4,
+    author: "John Steinbeck",
+    imageLink: "https://images.unsplash.com/photo-1476275466078-4007374efbbe?q=80&w=1429&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh",
+    pages: 160,
+    title: "The Epic Of Gilgamesh",
+    isbn: "978-0140441000",
+    category: "Mythology",
+    price: 14.99
+  },
+  {
+    id: 5,
+    author: "C.S. Lewis",
+    imageLink: "https://images.unsplash.com/photo-1588666309990-d68f08e3d4a6?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/Book_of_Job",
+    pages: 176,
+    title: "The Book Of Job",
+    isbn: "978-0192835001",
+    category: "Religious Text",
+    price: 10.99
+  },
+  {
+    id: 6,
+    author: "Sun Tzu",
+    imageLink: "https://images.unsplash.com/photo-1562232573-0305012a8818?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights",
+    pages: 288,
+    title: "One Thousand and One Nights",
+    isbn: "978-0140442892",
+    category: "Folklore",
+    price: 16.99
+  },
+  {
+    id: 7,
+    author: "George Orwell",
+    imageLink: "https://images.unsplash.com/photo-1589998059171-988d887df646?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://en.wikipedia.org/wiki/1984_(novel)",
+    pages: 328,
+    title: "1984",
+    isbn: "978-0451524935",
+    category: "Dystopian",
+    price: 13.99
+  },
+  {
+    id: 8,
+    author: "Harper Lee",
+    imageLink: "https://covers.openlibrary.org/b/isbn/9780064471190-L.jpg",
+    link: "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird",
+    pages: 281,
+    title: "To Kill a Mockingbird",
+    isbn: "978-0061120084",
+    category: "Fiction",
+    price: 14.50
+  },
+  {
+    id: 9,
+    author: "F. Scott Fitzgerald",
+    imageLink:"https://covers.openlibrary.org/b/isbn/9780141439556-L.jpg",
+    link: "https://en.wikipedia.org/wiki/The_Great_Gatsby",
+    pages: 180,
+    title: "The Great Gatsby",
+    isbn: "978-0743273565",
+    category: "Classic",
+    price: 11.99
+  },
+  {
+    id: 10,
+    author: "J.K. Rowling",
+    imageLink:  "https://covers.openlibrary.org/b/isbn/9780590353427-L.jpg",
+    link: "https://en.wikipedia.org/wiki/Harry_Potter",
+    pages: 309,
+    title: "Harry Potter and the Sorcerer’s Stone",
+    isbn: "978-0439708180",
+    category: "Fantasy",
+    price: 20.99
+  },
+  {
+    id: 11,
+    author: "Jane Austen",
+    imageLink: "https://covers.openlibrary.org/b/isbn/9780399590504-L.jpg",
+    link: "https://en.wikipedia.org/wiki/Pride_and_Prejudice",
+    pages: 800,
+    title: "Pride and Prejudice",
+    isbn: "978-0143105428",
+    category: "Memoir",
+    price: 9.99
+  },
+  {
+    id: 12,
+    isbn: "978-0399590504",
+    title: "Educated",
+    category: "Romance",
+    imageLink: "https://covers.openlibrary.org/b/isbn/9781503290563-L.jpg",
+    pages: 279,
+    author: "Jane Austen",
+    price: 6.99
+  },
+];
